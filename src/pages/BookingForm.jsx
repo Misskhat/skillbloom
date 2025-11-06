ce5ba055367d3eddb8f85b598c6686e9550b52de
Misskhat <misskhat.hossen@gmail.com>
2025-11-06 09:08:25 +0600

    tost conditionally apply when booking form

--- src/pages/BookingForm.jsx
+++ src/pages/BookingForm.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import {Link, useLoaderData, useParams} from "react-router";
+import {toast, ToastContainer} from "react-toastify";
 
 const BookingForm = () => {
     const {id} = useParams();
-    const handleBookig = (e) => {
+    const handleBooking = (e) => {
         e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        if (email && password) {
+            toast.success("Thank you for you booking");
+        } else {
+            toast.error("Please fill-up the email and password");
+        }
     };
     return (
         <div className="w-11/12 mx-auto my-5 space-y-5">
@@ -13,12 +21,11 @@ const BookingForm = () => {
                 <div className={`flex items-center justify-center`}>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-10">
                         <h1 className="text-center font-bold text-2xl"> Booking Your Seat </h1>
-                        <form onSubmit={handleBookig} className="card-body">
+                        <form onSubmit={handleBooking} className="card-body">
                             <fieldset className="fieldset">
                                 {/* email section */}
                                 <label className="label">Email</label>
                                 <input type="email" className="input" placeholder="Email" name="email" />
-
                                 {/* password section */}
                                 <label className="label">Password</label>
                                 <input
@@ -28,11 +35,13 @@ const BookingForm = () => {
                                     placeholder="Password"
                                     name="password"
                                 />
-
                                 <div>
                                     <a className="link link-hover">Forgot password?</a>
                                 </div>
-                                <button className="btn btn-neutral mt-4">Booking your seat</button>
+                                <button type="submit" className="btn btn-neutral mt-4">
+                                    Booking your seat
+                                </button>
+                                <ToastContainer></ToastContainer>
                             </fieldset>
                         </form>
                         {/* now working properly ==> need to talk conceptual section */}
